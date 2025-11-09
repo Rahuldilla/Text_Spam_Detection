fa89a7a12e0257bd7f81c21c56ef320c339209d5
RAHUL DILLA <120627658+Rahuldilla@users.noreply.github.com>
2025-11-09 19:10:19 +0530

    Clean up nltk.download calls in app.py
    Removed redundant nltk.download calls for 'punkt' and 'stopwords'.

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
